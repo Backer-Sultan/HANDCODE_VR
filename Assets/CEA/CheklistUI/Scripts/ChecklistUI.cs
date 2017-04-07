10206819a09ec325ac789743f6df9a6fa386f1e9
adrien.girard <adrien.girard@cea.fr>
2017-04-07 19:08:39 +0200

    Checklist based on UI componants

---
+++ Assets/CEA/CheklistUI/Scripts/ChecklistUI.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class ChecklistUI : MonoBehaviour {
+
+  public RectTransform m_Frame;
+
+  // Use this for initialization
+  void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    Reshape();
+
+  }
+
+  public void Reshape()
+  {
+    RectTransform title = m_Frame.GetChild(0).GetComponent<RectTransform>();
+    title.sizeDelta = new Vector2(m_Frame.sizeDelta.x, title.sizeDelta.y);
+
+    RectTransform spacer = m_Frame.GetChild(m_Frame.childCount-1).GetComponent<RectTransform>();
+    float height = m_Frame.sizeDelta.y - m_Frame.childCount - 1 * (title.sizeDelta.y + m_Frame.GetComponent<VerticalLayoutGroup>().spacing);
+    spacer.sizeDelta = new Vector2(m_Frame.sizeDelta.x, height);
+
+    for (int i = 1; i < m_Frame.childCount-1; i++)
+    {
+      RectTransform checkbox = m_Frame.GetChild(i).GetChild(0).GetComponent<RectTransform>();
+      checkbox.sizeDelta = new Vector2(title.sizeDelta.y, title.sizeDelta.y);
+      RectTransform item = m_Frame.GetChild(i).GetChild(1).GetComponent<RectTransform>();
+      float spacing = m_Frame.GetChild(i).GetComponent<HorizontalLayoutGroup>().spacing;
+      item.sizeDelta = new Vector2(m_Frame.sizeDelta.x - title.sizeDelta.y - spacing, title.sizeDelta.y);
+    }
+
+
+  }
+
+  public void SetTitle(string title)
+  {
+    Text titleShadow = m_Frame.GetChild(0).GetChild(0).GetChild(0).GetComponent<Text>();
+    Text titleText = titleShadow.transform.GetChild(0).GetComponent<Text>();
+    titleShadow.text = title;
+    titleText.text = title;
+  }
+
+  public string GetTitle()
+  {
+    Text titleShadow = m_Frame.GetChild(0).GetChild(0).GetChild(0).GetComponent<Text>();
+    return titleShadow.text;
+  }
+
+  public void SetItemText(int index, string text)
+  {
+    if(index< GetItemCount())
+    {
+      Text textShadow = m_Frame.GetChild(index + 1).GetChild(1).GetChild(0).GetComponent<Text>();
+      Text textItem = textShadow.transform.GetChild(0).GetComponent<Text>();
+
+      textShadow.text = text;
+      textItem.text = text;
+    }
+  }
+
+  public string GetItemText(int index)
+  {
+    if (index < GetItemCount())
+    {
+      Text textShadow = m_Frame.GetChild(index + 1).GetChild(1).GetChild(0).GetComponent<Text>();
+      return textShadow.text;
+    }
+    else
+      return "";
+  }
+
+  public void SetItemCheck(int index, bool check)
+  {
+    if (index < GetItemCount())
+    {
+      GameObject checkmark = m_Frame.GetChild(index + 1).GetChild(0).GetChild(0).gameObject;
+      checkmark.SetActive(check);
+    }
+  }
+
+  public bool GetItemCheck(int index)
+  {
+    if (index < GetItemCount())
+    {
+      GameObject checkmark = m_Frame.GetChild(index + 1).GetChild(0).GetChild(0).gameObject;
+      return checkmark.activeInHierarchy;
+    }
+    return false;
+  }
+
+  public GameObject AddItem()
+  {
+    Transform itemRef = m_Frame.GetChild(1);
+    GameObject item = Object.Instantiate(itemRef.gameObject, m_Frame);
+
+    item.transform.SetSiblingIndex(m_Frame.childCount - 2);
+    item.name = "Item " + (m_Frame.childCount-3);
+
+    SetItemText(GetItemCount() - 1, item.name);
+
+    return item;
+  }
+
+  public int GetItemCount()
+  {
+    return m_Frame.childCount - 2;
+  }
+
+
+}
---
+++ Assets/CEA/CheklistUI/Scripts/Editor/ChecklistUIEditor.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+[CustomEditor(typeof(ChecklistUI))]
+public class ChecklistUIEditor : Editor
+{
+
+  private ChecklistUI m_Checklist;
+
+  // Use this for initialization
+  void OnEnable()
+  {
+    m_Checklist = (ChecklistUI)target;
+    m_Checklist.m_Frame = m_Checklist.transform.GetChild(0).GetComponent<RectTransform>();
+  }
+
+  public override void OnInspectorGUI()
+  {
+
+    //Title Update
+    Transform frame = m_Checklist.m_Frame;
+
+    m_Checklist.SetTitle(EditorGUILayout.TextField("Checklist Title", m_Checklist.GetTitle()));
+    
+    int nbItems = EditorGUILayout.IntField("Number of UI elements:", m_Checklist.GetItemCount());
+
+    if (nbItems < 1)
+      return;
+
+    for (int i = 0; i < nbItems; i++)
+    {
+      if (i >= m_Checklist.GetItemCount())
+        m_Checklist.AddItem();
+
+      GUILayout.Label("Item " + i);
+      EditorGUI.indentLevel++;
+      GUILayout.BeginHorizontal();
+      m_Checklist.SetItemCheck(i,EditorGUILayout.Toggle(m_Checklist.GetItemCheck(i), GUILayout.Width(30)));
+      GUILayout.Label("Text", GUILayout.Width(55));
+      m_Checklist.SetItemText(i, EditorGUILayout.TextField(m_Checklist.GetItemText(i), GUILayout.ExpandWidth(true)));
+      GUILayout.EndHorizontal();
+      EditorGUI.indentLevel--;
+
+    }
+
+    while (m_Checklist.GetItemCount() > nbItems)
+      Destroy(m_Checklist.m_Frame.GetChild(m_Checklist.GetItemCount()));
+
+    if (GUI.changed)
+    {
+      EditorUtility.SetDirty(target);
+      m_Checklist.Reshape();
+    }
+
+    if (GUILayout.Button("Check/Uncheck all"))
+    {
+      bool check = !m_Checklist.GetItemCheck(0);
+      for (int i = 0; i < m_Checklist.GetItemCount(); i++)
+        m_Checklist.SetItemCheck(i, check);
+    }
+
+  }
+
+  private void Destroy(Transform trans)
+  {
+    while (trans.childCount != 0)
+      Destroy(trans.GetChild(0));
+
+    GameObject.DestroyImmediate(trans.gameObject);
+  }
+}
