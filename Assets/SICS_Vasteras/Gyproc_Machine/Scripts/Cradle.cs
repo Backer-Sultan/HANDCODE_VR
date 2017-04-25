21223e7e4104e6a09c1eabfc8ee0946aa453d4da
Backer-Sultan <backer.su@gmail.com>
2017-04-25 16:16:18 +0200

    Creating machine hierarchy and Cradle script.

---
+++ Assets/SICS_Vasteras/Gyproc_Machine/Scripts/Cradle.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cradle : MonoBehaviour
+{
+    // fields & properties
+
+    public float maxLeftPosX = -2.355f, maxRightPosX = 2.355f, middlePosX = 0f;
+    public float speed = 0.5f;
+    public bool isCradleInMaxRight { get { return (transform.localPosition.x >= maxRightPosX) ? true : false; } }
+    public Transform pinsherRotator;
+    [HideInInspector]
+    public bool isCradleMoving = false;
+    [HideInInspector]
+    public bool isBreakApplied = true;
+    [HideInInspector]
+    public bool isPinsherLow = false;
+    [HideInInspector]
+    public AudioSource audioSource;
+    [HideInInspector]
+    public Machine machine;
+
+    private Vector3 destination;
+    private Animator pinsherAnimator;
+
+
+
+    // methods & coroutines
+
+    private void Start()
+    {
+        if (!pinsherRotator)
+            pinsherRotator = transform.Find("PinsherRotator");
+        if (!pinsherRotator)
+            Debug.LogError("Cradle.cs: Object PinsherRotator is missing!");
+        pinsherAnimator = pinsherRotator.GetComponent<Animator>();
+        if (!pinsherAnimator)
+            Debug.LogError("Crale.cs: Animator Component is missing on object PinsherRotator!");
+    }
+
+    public IEnumerator MoveToRight()
+    {
+        // sound processing
+        audioSource.clip = machine.sounds.Cradle_Moving;
+        audioSource.Play();
+        //
+        destination = (transform.localPosition.x < middlePosX) ? new Vector3(middlePosX, 0f, 0f) : new Vector3(maxRightPosX, 0f, 0f);
+        while(Vector3.Distance(transform.position, destination) > Vector3.kEpsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.localPosition, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    public IEnumerator MoveToLeft()
+    {
+        // sound processing
+        audioSource.clip = machine.sounds.Cradle_Moving;
+        audioSource.Play();
+        //
+        destination = (transform.localPosition.x > middlePosX) ? new Vector3(middlePosX, 0f, 0f) : new Vector3(maxLeftPosX, 0f, 0f);
+        while (Vector3.Distance(transform.position, destination) > Vector3.kEpsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.localPosition, destination, speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    public void Stop()
+    {
+        // sound processing
+        audioSource.clip = machine.sounds.Cradle_Stopping;
+        audioSource.Play();
+        //
+        StopAllCoroutines();
+    }
+
+    public void LowerPinsher()
+    {
+        isPinsherLow = true;
+        pinsherAnimator.SetBool("LowerPincher", true);
+    }
+
+    public void RaisePinsher()
+    {
+        isPinsherLow = false;
+        pinsherAnimator.SetBool("LowerPincher", false);
+    }
+
+    public void ToggleBreak()
+    {
+        // sound processing
+        audioSource.clip = machine.sounds.Cradle_BreakToggle;
+        audioSource.Play();
+        //
+        isBreakApplied = !isBreakApplied;
+
+        /******* depended on script PaperConsole.cs *****/
+        //machine.paperConsole.breakButton.GetComponent<Animator>().SetBool("isBreakApplied", isBreakApplied);
+    }
+
+}
---
+++ Assets/SICS_Vasteras/Gyproc_Machine/Scripts/Sounds_Machine.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds_Machine : MonoBehaviour
+{
+    public AudioClip Cradle_Moving, Cradle_Stopping, Cradle_BreakToggle;
+}
