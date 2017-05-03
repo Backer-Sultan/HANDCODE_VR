88ab9c23bcc2fa3f2e0a895ac032adec07ab8730
adrien.girard <adrien.girard@cea.fr>
2017-05-03 18:31:22 +0200

    Buttons example added Simple button, toggle button and rotary switch button added.

---
+++ Assets/CEA/Buttons/Scripts/FollowMouseCursor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouseCursor : MonoBehaviour {
+
+  public float distanceFromCamera = 10;
+
+	public float pressureSpeed = 0.05f;
+
+	private float pressure;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    Vector3 pos = Input.mousePosition;
+		if(Input.GetMouseButton(0))
+		{
+			pressure += pressureSpeed;
+			if (pressure > 3)
+				pressure = 3;
+		}
+		else
+		{
+			pressure -= 2*pressureSpeed;
+			if (pressure < 0)
+				pressure = 0;
+
+		}
+
+    pos.z = distanceFromCamera+ pressure;
+    pos = Camera.main.ScreenToWorldPoint(pos);
+    transform.position = pos;
+  }
+}
---
+++ Assets/CEA/Buttons/Scripts/PhysxRotaryButton.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PhysxRotaryButton : MonoBehaviour {
+
+	public HingeJoint joint;
+	[Range (2,12)]
+	public int numberOfPositions = 2;
+	[Range(1, 360)]
+	public int rotationAmplitude = 90;
+
+	private int currentPositionId = 0;
+	public int CurrentPosition
+	{
+		get { return this.currentPositionId; }
+	}
+
+	private JointSpring spring;
+
+	public EventTrigger.TriggerEvent onPhysxButtonSwitch;
+
+	// Use this for initialization
+	void Start () {
+
+		if (joint != null)
+		{
+			JointLimits limits = new JointLimits();
+			limits.min = -rotationAmplitude / 2.0f;
+			limits.max = rotationAmplitude / 2.0f;
+			limits.bounciness = 0;
+			limits.bounceMinVelocity = 0.2f;
+			limits.contactDistance = 0;
+			joint.limits = limits;
+
+			spring = new JointSpring();
+			spring.damper = 10;
+			spring.spring = 100;
+			spring.targetPosition = -rotationAmplitude / 2.0f;
+			joint.spring = spring;
+
+			transform.localRotation = Quaternion.AngleAxis(spring.targetPosition, Vector3.up);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (joint != null)
+		{
+			float buttonAngle = transform.localRotation.eulerAngles.y - joint.connectedBody.transform.localRotation.eulerAngles.y;
+			if (buttonAngle > 180)
+				buttonAngle -=360 ;
+
+			//Debug.Log(buttonAngle);
+			float stepNotch = rotationAmplitude / (float)(numberOfPositions-1);
+			for (int i = 0;i< numberOfPositions;i++)
+			{
+				if (i != currentPositionId)
+				{
+					float notchAngle = -rotationAmplitude / 2.0f + i * stepNotch;
+					if (Mathf.Abs(buttonAngle - notchAngle) < stepNotch / 2.0f)
+					{
+						spring.targetPosition = notchAngle;
+						joint.spring = spring;
+						currentPositionId = i;
+						BaseEventData eventData = new BaseEventData(EventSystem.current);
+						onPhysxButtonSwitch.Invoke(eventData);
+					}
+				}
+			}
+		}
+	}
+}
