063fd9e59862e34c2bf1c020144b081e11f40cdc
Backer-Sultan <backer.su@gmail.com>
2017-05-10 17:18:27 +0200

    Working on MainConsole

---
+++ Assets/SICS_Vasteras/Gyproc_Machine/_Scripts/SpoolDamage.cs
@@ -0,0 +1,19 @@
+﻿/*********************************************
+ * Project: HANDCODE                         *
+ * Author:  Backer Sultan                    *
+ * Email:   backer.sultan@ri.se              *
+ * *******************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpoolDamage : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "ArmCone")
+            SendMessageUpwards("ApplyDamage", SendMessageOptions.RequireReceiver);
+    }
+}
