8e2a0036f0d4e06d42fe7d8a92d99ee0f7a671bf
PetterSICS <petter@sics.se>
2017-05-19 13:48:43 +0200

    Line renderer kraken

---
+++ Assets/SICS_Vasteras/something.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class something : MonoBehaviour
+{
+
+	
+	
+	void Update ()
+    {
+        if (transform.position.y <= -2f)
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+    }
+
+
+  
+}
+
+
