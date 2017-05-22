c8b3a607916b0523dc7bd206fd8e57dabf2ff511
Backer-Sultan <backer.su@gmail.com>
2017-05-22 18:35:25 +0200

    Adding ProgressRadial Script + tests

---
+++ Assets/SICS_Vasteras/UI/ProgressRadial/Scripts/ProgressRadialTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressRadialTest : MonoBehaviour
+{
+    public ProgressRadial progressRadial;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            progressRadial.Add(0.1f);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            progressRadial.Subtract(0.1f);
+    }
+
+}
