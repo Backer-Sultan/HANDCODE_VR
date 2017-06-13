a9b6b866032a47f202b576a441e29bd225c89796
adrien.girard <adrien.girard@cea.fr>
2017-06-13 18:08:51 +0200

    Paper Flow Demo Added

---
+++ Assets/CEA/PaperFlow/Scripts/CradleKeyboardControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CradleKeyboardControl : MonoBehaviour {
+
+	public Cradle cradle;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow))
+			cradle.MoveToRight();
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+			cradle.MoveToLeft();
+	}
+}
