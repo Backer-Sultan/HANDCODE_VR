d467542f24a58d7296a7b5c2996917ef426dc6aa
Backer Sultan <Backer-Sultan@users.noreply.github.com>
2017-06-15 09:21:04 +0200

    Getting new modules

---
+++ Assets/SICS_Vasteras/UI/UI_01_in_progress/Teleportation/teleportation_02/02/Scripts/ScrollTexture.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollTexture : MonoBehaviour
+{
+	public GameObject[] parts;
+	Renderer[] rends;
+	public float speed =1f;
+
+	private void Start()
+	{
+		rends = GetComponentsInChildren<Renderer>();
+	}
+
+
+	public void RunPaper()
+	{
+		foreach(Renderer r in rends)
+		{
+			r.material.mainTextureOffset = new Vector2(r.material.mainTextureOffset.y - Time.deltaTime*speed, r.material.mainTextureOffset.x);
+		}
+	}
+
+
+	private void FixedUpdate()
+
+	{
+		RunPaper();
+	} 
+}
