42f9b358fe7b326ee8d266c698d3f699d00474f9
Backer-Sultan <backer.su@gmail.com>
2017-06-15 11:57:03 +0200

    saving updates

---
+++ Assets/ArrowSpawnerDestroyer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowSpawnerDestroyer : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+    [Tooltip("You need to exit play mode and start again to catch the change!")]
+    [Range(0.1f, 5f)]
+    public float SpawnsPerSecond = 0.5f;
+    private WaitForSeconds wait;
+
+    private void Start()
+    {
+        wait = new WaitForSeconds(1f / SpawnsPerSecond);
+        StartCoroutine(SpawnRoutine());
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            yield return wait;
+            GameObject newArrow = GameObject.Instantiate(arrowPrefab, transform.parent);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name.StartsWith("DirectionIndictatorArrow"))
+        {
+            GameObject.Destroy(other.gameObject);
+        }
+    }
+
+    private void OnValidate()
+    {
+        wait = new WaitForSeconds(1f / SpawnsPerSecond);
+    }
+}
---
+++ Assets/DirectionIndicatorAinmator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DirectionIndicatorAinmator : MonoBehaviour
+{
+    [Range(0f, 1f)]
+    public float speed = 0.5f;
+
+    private void Update()
+    {
+         transform.Translate(Vector3.up * speed * Time.deltaTime, Space.Self);
+    }
+}
