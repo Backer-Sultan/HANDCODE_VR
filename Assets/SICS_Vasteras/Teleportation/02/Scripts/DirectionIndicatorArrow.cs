233a7357ac20965d9cebd7261c52aa296039bed4
Backer-Sultan <backer.su@gmail.com>
2017-06-20 17:13:15 +0200

    Impoving teleportatoin

---
+++ Assets/SICS_Vasteras/Teleportation/02/Scripts/DirectionIndicatorArrow.cs
@@ -0,0 +1,38 @@
+﻿/*********************************************
+ * Project: HANDCODE                         *
+ * Author:  Backer Sultan                    *
+ * Email:   backer.sultan@ri.se              *
+ * *******************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionIndicatorArrow : MonoBehaviour
+{
+    public float speed = 0.2f;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        // initialization
+        animator = GetComponent<Animator>();
+        if (animator == null)
+            Debug.LogError("DirectionIndicatorArrow.cs: component 'Animator' is missing!");
+    }
+
+    private void Update()
+    {
+        transform.localPosition += Vector3.up * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "DestroyTrigger")
+        {
+            animator.SetTrigger("FadeOut");
+            Destroy(gameObject, 1f);
+        }
+    }
+}
---
+++ Assets/SICS_Vasteras/Teleportation/02/Scripts/DirectionIndicatorArrowSpawner.cs
@@ -0,0 +1,53 @@
+﻿/*********************************************
+ * Project: HANDCODE                         *
+ * Author:  Backer Sultan                    *
+ * Email:   backer.sultan@ri.se              *
+ * *******************************************/
+
+using System.Collections;
+using UnityEngine;
+
+public class DirectionIndicatorArrowSpawner : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+    [Tooltip("You need to exit play mode and start again to catch the change!")]
+    [Range(0.1f, 5f)]
+    public float SpawnsPerSecond = 0.5f;
+
+    private WaitForSeconds wait;
+
+   
+    private void Start()
+    {
+        //initialization
+        if (arrowPrefab == null)
+            Debug.LogError("DirectionIndicatorArrowSpawner.cs: arrowPrefab is not assigned!");
+        wait = new WaitForSeconds(1f / SpawnsPerSecond);
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            GameObject newArrow = Instantiate(arrowPrefab, transform, false);
+            yield return wait;
+        }
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    private void OnDisable()
+    {
+        foreach(DirectionIndicatorArrow arrow in GetComponentsInChildren<DirectionIndicatorArrow>())
+        {
+            StopAllCoroutines();
+            if(arrow.name.Contains("(Clone)"))
+            {
+                Destroy(arrow.gameObject);
+            }
+        }
+    }
+}
