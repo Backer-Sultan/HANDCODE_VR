659e5ab48a49c85fa7baf7b41d3d2cc01c0052f5
Backer-Sultan <backer.su@gmail.com>
2017-06-29 12:02:24 +0200

    Pushing UI_Button without using physics - test

---
+++ Assets/SICS_Vasteras/UI/Button_Play/UI_ButtonSuface.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_ButtonSuface : MonoBehaviour
+{
+    private float initalPosition;
+    private float maxPushPosition = 0f;
+    private new Collider collider;
+    private bool isFollowingFinger = false;
+
+    private void Start()
+    {
+        initalPosition = transform.localPosition.z;
+        collider = GetComponent<Collider>();
+        if(collider == null)
+            print("UI_ButtonSurface: Component 'Collider' is missing!");
+        print(initalPosition);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Background")
+            isFollowingFinger = false;
+        if (other.tag == "Finger")
+            isFollowingFinger = true;
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(transform.localPosition.z <= maxPushPosition&& transform.localPosition.z >= initalPosition)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.InverseTransformVector(other.transform.position).z);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Finger")
+            isFollowingFinger = false;
+    }
+}
