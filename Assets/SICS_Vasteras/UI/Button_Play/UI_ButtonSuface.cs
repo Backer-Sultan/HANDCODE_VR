09ee6b1bf08ff01566f03a26cebc039097a391dc
Backer-Sultan <backer.su@gmail.com>
2017-06-30 16:46:03 +0200

    Updating the cradle's prefab

--- Assets/SICS_Vasteras/UI/Button_Play/UI_ButtonSuface.cs
+++ Assets/SICS_Vasteras/UI/Button_Play/UI_ButtonSuface.cs
@@ -12,63 +12,74 @@ public class UI_ButtonSuface : MonoBehaviour
     private  Rigidbody rigid;
     private bool isFollowingFinger = false;
 
-    private void Start()
-    {
-        initalPosition = transform.localPosition.z;
-        collider = GetComponent<Collider>();
-        rigid = GetComponent<Rigidbody>();
-        if (collider == null)
-            print("UI_ButtonSurface: Component 'Collider' is missing!");
-    }
+    //private void Start()
+    //{
+    //    initalPosition = transform.localPosition.z;
+    //    collider = GetComponent<Collider>();
+    //    rigid = GetComponent<Rigidbody>();
+    //    if (collider == null)
+    //        print("UI_ButtonSurface: Component 'Collider' is missing!");
+    //}
 
-    // reaching max push
-    private void OnTriggerEnter(Collider other)
-    {
-        print("OnTriggerEnter!");
-        if (other.name == "Background")
-            rigid.isKinematic = true;
-    }
+    //// reaching max push
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    print("OnTriggerEnter!");
+    //    if (other.name == "Background")
+    //        rigid.isKinematic = true;
+    //}
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        print("OnCollisionEnter!"); 
-        StopAllCoroutines();
-    }
-    private void OnCollisionExit(Collision collision)
-    {
-        print("OnCollisionExit!");
-        if(collider.gameObject.tag == "Finger")
-        {
-            rigid.isKinematic = false;
-            StartCoroutine(MoveSurfaceBackRoutine());
-        }
-    }
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    print("OnCollisionEnter!"); 
+    //    StopAllCoroutines();
+    //}
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    print("OnCollisionExit!");
+    //    if(collider.gameObject.tag == "Finger")
+    //    {
+    //        rigid.isKinematic = false;
+    //        StartCoroutine(MoveSurfaceBackRoutine());
+    //    }
+    //}
 
-    private IEnumerator MoveSurfaceBackRoutine()
-    {
-        while(transform.localPosition.z > initalPosition)
-        {
-            transform.Translate(Vector3.back * Time.deltaTime, Space.Self);
-            yield return null;
-        }
-    }
+    //private IEnumerator MoveSurfaceBackRoutine()
+    //{
+    //    while(transform.localPosition.z > initalPosition)
+    //    {
+    //        transform.Translate(Vector3.back * Time.deltaTime, Space.Self);
+    //        yield return null;
+    //    }
+    //}
+
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    print("OnTriggerStay!");
+    //    if(isFollowingFinger)
+    //    {
+    //        Vector3 localPosition = transform.InverseTransformPoint(other.transform.position);
+    //        localPosition.Set(0f, 0f, localPosition.z);
+    //        transform.localPosition = localPosition;
+
+    //        Vector3 localVelocity = transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity);
+    //        localVelocity.x = 0f;
+    //        localVelocity.y = 0f;
+
+    //        GetComponent<Rigidbody>().velocity = transform.TransformDirection(localVelocity);
+    //    }
+    //}
+
 
     private void OnTriggerStay(Collider other)
     {
-        print("OnTriggerStay!");
-        if(isFollowingFinger)
+        if(other.tag == "Finger")
         {
-            Vector3 localPosition = transform.InverseTransformPoint(other.transform.position);
-            localPosition.Set(0f, 0f, localPosition.z);
-            transform.localPosition = localPosition;
+            print("triggering!!!");
+            //transform.localPosition = transform.InverseTransformPoint(other.transform.position);
+            transform.position = new Vector3(transform.position.x, transform.position.y, other.transform.position.z);
 
-            Vector3 localVelocity = transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity);
-            localVelocity.x = 0f;
-            localVelocity.y = 0f;
-
-            GetComponent<Rigidbody>().velocity = transform.TransformDirection(localVelocity);
         }
     }
-
 
 }
