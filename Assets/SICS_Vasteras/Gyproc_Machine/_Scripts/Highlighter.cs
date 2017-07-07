347a61a7c6967bac2533f19a24b939e6f95c4f59
Backer Sultan <Backer-Sultan@users.noreply.github.com>
2017-07-07 13:02:31 +0200

    Feat/highlight (#23)
    * Adding 'Highlighter' script
    
    To highlight a game object:
    
    1- Attach 'Highlighter.cs' to the object you want to highlight.
    2- Make sure "Emission" is enabled on the children-objects' shaders that
    you want to highlight.
    3- Enable the script.
    
    To unhighlight a game object:
    1- Disable the script.
    
    * Updating the button on the scene with the new hightlight
    
    * Updating InteraciveObject.cs

---
+++ Assets/SICS_Vasteras/Gyproc_Machine/_Scripts/Highlighter.cs
@@ -0,0 +1,51 @@
+﻿/*********************************************
+ * Project: HANDCODE                         *
+ * Author:  Backer Sultan                    *
+ * Email:   backer.sultan@ri.se              *
+ * *******************************************/
+
+using System.Collections;
+using UnityEngine;
+
+
+public class Highlighter : MonoBehaviour
+{
+    /* fields & properties */
+
+    public Color highlightColor = Color.blue;
+
+    private Renderer[] renderers;
+    private Color emissionColor;
+
+
+
+    /* methods & coroutines */
+
+    private void OnEnable()
+    {
+        renderers = GetComponentsInChildren<Renderer>();
+        emissionColor = Color.blue;
+        StartCoroutine(HighlightRoutine());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+        foreach (Renderer rend in renderers)
+            rend.material.SetColor("_EmissionColor", Color.black);
+    }
+
+    private IEnumerator HighlightRoutine()
+    {
+        float emission;
+        Color finalColor;
+        while (true)
+        {
+            emission = Mathf.PingPong(Time.time, 1f);
+            finalColor = highlightColor * Mathf.LinearToGammaSpace(emission);
+            foreach (Renderer rend in renderers)
+                rend.material.SetColor("_EmissionColor", finalColor);
+            yield return null;
+        }
+    }
+}
