bdfec56f62eeee6281c181109c8f19b210ea1099
Backer-Sultan <backer.su@gmail.com>
2017-07-07 13:00:44 +0200

    Updating InteraciveObject.cs

--- Assets/SICS_Vasteras/Gyproc_Machine/_Scripts/InteractiveObject.cs
+++ Assets/SICS_Vasteras/Gyproc_Machine/_Scripts/InteractiveObject.cs
@@ -6,63 +6,34 @@ public class InteractiveObject : MonoBehaviour
 {
    
     public bool isHighlighted { get { return _isHighlighted; } }
-    public Renderer highLightedObject;
-    public new Light light;
-
+    
     private bool _isHighlighted = false;
-    private Color defColor;
-    private Color minAlphaColor;
-    private Color maxAlphaColor;
-    public bool lerpAphaRoutineIsRunning = false;
+    private Highlighter highlighter;
+
 
     internal void Start()
     {
-        if (light == null)
-            Debug.LogError("InteractableObject.cs: No light component is found on this object!");
-
-        if (highLightedObject == null)
-        {
-            Debug.LogError("InteractableObject.cs: 'Highlighted Object is not specified!");
-            return;
-        }
-        defColor = highLightedObject.material.color;
-        minAlphaColor = new Color(defColor.r, defColor.g, defColor.b, 0.1f);
-        maxAlphaColor = new Color(defColor.r, defColor.g, defColor.b, 0.9f);
+        highlighter = GetComponentInChildren<Highlighter>();
+        if (highlighter == null)
+            Debug.LogError("InteractiveObject.cs: No Highlighter script found in the object's hierarchy!");
     }
 
     public void Highlight()
     {
-        if (!lerpAphaRoutineIsRunning)
-        {
-            StartCoroutine(LerpAlpha());
-            _isHighlighted = true;
-            light.enabled = true;
-        }
+        highlighter.enabled = true;
+        _isHighlighted = true;
     }
 
     public void Unhighlight()
     {
-        print("Unhighlighted!");
-        StopAllCoroutines();
-        lerpAphaRoutineIsRunning = false;
-        highLightedObject.material.color = defColor;
+        highlighter.enabled = false;
         _isHighlighted = false;
-        light.enabled = false;
-    }
-
-    private IEnumerator LerpAlpha()
-    {
-        print("LerpAlpha Started!");
-        lerpAphaRoutineIsRunning = true;
-        while (true)
-        {
-            highLightedObject.material.color = Color.Lerp(minAlphaColor, maxAlphaColor, Mathf.PingPong(Time.time, 1));
-            Color color = Color.Lerp(minAlphaColor, maxAlphaColor, Mathf.PingPong(Time.time, 1));
-            yield return null;
-        }
     }
 
 
+
+
+    // for test only: 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
