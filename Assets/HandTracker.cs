c31e68c6b74b53885dda7a0340abb92c73ceed9a
Backer-Sultan <backer.su@gmail.com>
2017-10-10 15:09:49 +0200

    UI with VG rotation problem

---
+++ Assets/HandTracker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HandCode
+{
+    public class HandTracker : MonoBehaviour
+    {
+        public Transform pointToFollow;
+
+        private void Update()
+        {
+            transform.position = pointToFollow.position;
+            transform.rotation = pointToFollow.rotation;
+        }
+    }
+
+}
---
+++ Assets/Highlighter_UI.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HandCode
+{
+    public class Highlighter_UI : Highlighter
+    {
+        internal Image[] images;
+        internal Color[] initialColors;
+
+        internal override void Start()
+        {
+            base.Start();
+
+            images = GetComponentsInChildren<Image>();
+            GetInitialColors();
+        }
+
+        internal override void OnEnable()
+        {
+            Highlight();
+        }
+
+        internal override void OnDisable()
+        {
+            Unhighlight();
+        }
+
+        private void GetInitialColors()
+        {
+            if (images != null && images.Length > 0)
+            {
+                initialColors = new Color[images.Length];
+
+                for (int i = 0; i < images.Length; i++)
+                {
+                    initialColors[i] = images[i].color;
+                }
+            }
+        }
+
+        private void Highlight()
+        {
+            if (images != null)
+            {
+                for (int i = 0; i < images.Length; i++)
+                {
+                    images[i].color = highlightColor;
+                }
+            }
+        }
+
+        private void Unhighlight()
+        {
+            if (images != null)
+            {
+                for (int i = 0; i < images.Length; i++)
+                {
+                    images[i].color = initialColors[i];
+                }
+            }
+        }
+    }
+}
