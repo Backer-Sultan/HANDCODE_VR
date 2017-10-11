87965c82b2a1ddac08ab9d72876b39174d6abfe3
Backer Sultan <backer.su@gmail.com>
2017-10-11 14:16:41 +0200

    wip

---
+++ Assets/HS_Button_VG.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HandCode
+{
+    public class HS_Button_VG : MonoBehaviour
+    {
+        Animator animator;
+
+        private void Start()
+        {
+            animator = GetComponentInChildren<Animator>();
+        }
+
+        public void PlayClickAnimation()
+        {
+            animator.SetTrigger("Click");
+        }
+
+        public void PlaySlideAnimation()
+        {
+            animator.SetTrigger("Slide");
+        }
+        public void SetAudioAnimation(bool state)
+        {
+            animator.SetBool("Audio", state);
+        }
+    } 
+}
