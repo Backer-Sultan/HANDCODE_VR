4873a63cb87aef5c7c099e1eb70dbdd14ec77f03
Backer Sultan <backer.su@gmail.com>
2017-10-19 11:57:20 +0200

    Upgrading to Unity v2017.2.0f3
    Submodules are upgraded as well. Make sure to pull the updates in the
    supbmodule.

--- Assets/Oculus/OvrAvatar/Scripts/OvrAvatarLocalDriver.cs
+++ Assets/Oculus/OvrAvatar/Scripts/OvrAvatarLocalDriver.cs
@@ -43,7 +43,7 @@ public class OvrAvatarLocalDriver : OvrAvatarDriver {
 
     public override bool GetCurrentPose(out PoseFrame pose)
     {
-        Vector3 headPos = UnityEngine.VR.InputTracking.GetLocalPosition(UnityEngine.VR.VRNode.CenterEye);
+        Vector3 headPos = UnityEngine.XR.InputTracking.GetLocalPosition(UnityEngine.XR.XRNode.CenterEye);
 #if UNITY_ANDROID && !UNITY_EDITOR
         headPos.y += mobileBaseHeadHeight;
 #endif
@@ -52,7 +52,7 @@ public class OvrAvatarLocalDriver : OvrAvatarDriver {
         {
             voiceAmplitude = voiceAmplitude,
             headPosition = headPos,
-            headRotation = UnityEngine.VR.InputTracking.GetLocalRotation(UnityEngine.VR.VRNode.CenterEye),
+            headRotation = UnityEngine.XR.InputTracking.GetLocalRotation(UnityEngine.XR.XRNode.CenterEye),
             handLeftPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch),
             handLeftRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch),
             handRightPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch),
