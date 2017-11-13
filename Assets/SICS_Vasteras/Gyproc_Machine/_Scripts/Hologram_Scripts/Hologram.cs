9d66cddac3e61d27567558ca4dec79031043542e
Backer-Sultan <backer.su@gmail.com>
2017-11-13 13:10:22 +0100

    Updating Hologram & CradleHologram scripts

--- Assets/SICS_Vasteras/Gyproc_Machine/_Scripts/Hologram_Scripts/Hologram.cs
+++ Assets/SICS_Vasteras/Gyproc_Machine/_Scripts/Hologram_Scripts/Hologram.cs
@@ -14,23 +14,21 @@ namespace HandCode
         /* fields & properties */
 
         [Range(0f, 1f)]
-        public float moveSpeed = 0.5f;
+        public float movementSpeed = 0.5f;
         [Range(0f, 1f)]
-        public float rotateSpeed = 0.5f;
-        public float waitTime = 3f;  // time to wait before resetting the hologram to its initial position and rotation
+        public float rotationSpeed = 0.5f;
+        public float waitTime = 3f; // time to wait before resetting the hologram to its initial position and rotation
         public Color color1 = new Color(1f, 0.5f, 0.5f, 0.2f);
         public Color color2 = new Color(1f, 0.86f, 0f, 0.2f);
 
         internal Vector3 initialPosition; // stored as local position
-        internal Vector3 initialRotation; // stored as Eular angle
-        internal float initialMoveSpeed;
-        internal float initialRotateSpeed;
-        protected Transform[] children;
-        
+        internal Vector3 initialRotation; // stored as Eular angles
+        internal float initialMovementSpeed;
+        internal float initialRotationSpeed;
+        internal Transform[] children;
+
         private Color lerpedColor;
         private Renderer[] rends;
-        private bool resetPositionRoutineFlag;
-        private bool resetRotationRoutineFlag;
 
 
 
@@ -44,18 +42,18 @@ namespace HandCode
 
         protected virtual void Awake()
         {
-            // getting initial values for position, rotation and speed
+            // gettin ginitial values for position, rotation and speed
             initialPosition = transform.localPosition;
             initialRotation = transform.localEulerAngles;
-            initialMoveSpeed = moveSpeed;
-            initialRotateSpeed = rotateSpeed;
+            initialMovementSpeed = movementSpeed;
+            initialRotationSpeed = rotationSpeed;
 
-            // getting referenced to children
+            // getting references to children
             rends = GetComponentsInChildren<Renderer>(true);
             children = GetComponentsInChildren<Transform>(true);
         }
 
-        // general color lerp - make sure to call it from derived methods!
+        // general color lerp
         protected virtual void Update()
         {
             lerpedColor = Color.Lerp(color1, color2, Mathf.PingPong(Time.time * 2f, 1));
@@ -65,11 +63,11 @@ namespace HandCode
 
         protected virtual void OnEnable()
         {
-            foreach (Transform t in children)
-                t.gameObject.SetActive(true);
-            
             ResetPosition();
             ResetRotation();
+
+            foreach (Transform t in children)
+                t.gameObject.SetActive(true);
         }
 
         protected virtual void OnDisable()
@@ -78,7 +76,6 @@ namespace HandCode
             {
                 if (t == transform)
                     continue;
-
                 t.gameObject.SetActive(false);
             }
             StopAllCoroutines();
@@ -86,46 +83,14 @@ namespace HandCode
 
         protected virtual void ResetPosition()
         {
-            moveSpeed = initialMoveSpeed;
             transform.localPosition = initialPosition;
+            movementSpeed = initialMovementSpeed;
         }
 
         protected virtual void ResetRotation()
         {
-            rotateSpeed = initialRotateSpeed;
             transform.localEulerAngles = initialRotation;
-        }
-
-        protected virtual void ResetPositionAfter(float seconds)
-        {
-            // making sure to start one instanse of the coroutine `ResetPositionRoutine`
-            if (!resetPositionRoutineFlag)
-                StartCoroutine(ResetPositionRoutine(seconds));
-        }
-
-        protected virtual void ResetRotationAfter(float seconds)
-        {
-            // making sure to start one instanse of the coroutine `ResetRotationRoutine`
-            if (!resetRotationRoutineFlag)
-                StartCoroutine(ResetRotationRoutine(seconds));
-        }
-
-        private IEnumerator ResetPositionRoutine(float secondeToWait)
-        {
-            resetPositionRoutineFlag = true;
-            moveSpeed = 0f;
-            yield return new WaitForSeconds(secondeToWait);
-            ResetPosition();
-            resetPositionRoutineFlag = false;
-        }
-
-        private IEnumerator ResetRotationRoutine(float secondsToWait)
-        {
-            resetRotationRoutineFlag = true;
-            rotateSpeed = 0f;
-            yield return new WaitForSeconds(secondsToWait);
-            ResetRotation();
-            resetRotationRoutineFlag = false;
+            rotationSpeed = initialRotationSpeed;
         }
     }
 }
