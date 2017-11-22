bdb92c6fa096bb137cbec4be1374b320bc999d19
Backer Sultan <Backer-Sultan@users.noreply.github.com>
2017-11-22 11:17:23 +0100

    Fix/general bugs (#75)
    * fixing a bug in teleportation sysem.
    
    - The bug:
    The teleportation ring snaps on the teleportation trace line. when
    they collide with eachother.
    - The fix:
    All teleportation objects are added to the 'IgnoreRaycast' layer.
    
    * fixing two bugs
    
    Bug 1: a wrong voice-over for task: Move_To_Pos_3
    Bug 2: The hint line for the arms refers to a button instead of the arm
    
    * activating task: Paste_Double_Adhesive_Tape & adding a patch to reset
    spool after getting damage by pressing x on keyboard
    
    * reset spool automatically after it gets damaged

--- Assets/SICS_Vasteras/Gyproc_Machine/_Scripts/Machine.cs
+++ Assets/SICS_Vasteras/Gyproc_Machine/_Scripts/Machine.cs
@@ -143,7 +143,30 @@ namespace HandCode
                 print("haptics");
                 TriggerHaptics();
             }
-               
+
+
+            if (Input.GetKeyDown(KeyCode.X))
+                ResetSpoolState();
+        }
+
+        /* DIRTY PATCH */
+        // hardcoded reset for the spool and the arms to avoid restarting the game when damaging the spool.
+        public void ResetSpoolState()
+        {
+            spool_Right.transform.parent = transform;
+            spool_Right.transform.localPosition = Vector3.zero;
+            spool_Right.transform.localEulerAngles = Vector3.zero;
+            armRig_Right.arm_Left.transform.localPosition = new Vector3(0f, 0f, -0.2f);
+            armRig_Right.arm_Right.transform.localPosition = new Vector3(0f, 0f, 0.2f);
+            armRig_Right.arm_Left.speed = armRig_Right.arm_Right.speed = 0.1f;
+            spool_Right._isDamaged = false;
+            spool_Right._isHandled = false;
+            spool_Right.GetComponentInChildren<Renderer>().material.color = Color.white;
+        }
+
+        public void ResetSpoolStateAfter(float seconds)
+        {
+            Invoke("ResetSpoolState", seconds);
         }
     }
 }
